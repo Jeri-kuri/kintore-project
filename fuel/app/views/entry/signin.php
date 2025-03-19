5108760eb7a4e1613acf9f7a540af0fdb34f662c
Jeri-kuri <komiyajerico@gmail.com>
2025-03-19 12:19:12 +0900

    adding comments to the index page

--- fuel/app/views/entry/signin.php
+++ fuel/app/views/entry/signin.php
@@ -1,39 +1,36 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Signin</title>
-  <?php echo Asset::css('login-layout.css'); ?>
-</head>
-<body>
-     
+  <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Signin</title>
+    <?php echo Asset::css('login-layout.css'); ?>
+  </head>
+  <body>
+    <!--タイトルとサブタイトル-->
     <div class="title-container">
-        <h1 class="main-title">KIN TORE</h1>
-        <h2 class="subtitle">WELCOME NEW TRAINEE</h2>
-        <header>新規会員登録</header>
+      <h1 class="main-title">KIN TORE</h1>
+      <h2 class="subtitle">WELCOME NEW TRAINEE</h2>
+      <header>新規会員登録</header>
     </div>  
-
+    <!--会員登録フォームのコンテナ-->
     <div class="container">
-        
-    <input type="checkbox" id="check">
-    <div class="login form">
-      <form action="<?php echo Uri::create('signin/create'); ?>" method="POST">
-        <h3>ユーザーネーム</h3>
-        <input type="text" name="username" required>
-        <h3>パスワード</h3>
-        <input type="password" name="password" required>
-        <input type="submit" class="button" value="登録">
-      </form>
-      
+      <input type="checkbox" id="check">
+      <div class="login form">
+        <!--サインインフォーム-->
+        <form action="<?php echo Uri::create('signin/create'); ?>" method="POST">
+          <h3>ユーザーネーム</h3>
+          <input type="text" name="username" required>
+          <h3>パスワード</h3>
+          <input type="password" name="password" required>
+          <input type="submit" class="button" value="登録">
+        </form>
+      </div> 
     </div>
-    
-  </div>
+    <!--ログインページへのリンク-->
     <div class="signup">
         <span class="signup">
          <a for="check" href="<?php echo Uri::create('login'); ?>">ログインはこちら</a>
         </span>
     </div>
-  
-
-</body>
+  </body>
 </html>
--- fuel/app/views/goal/index.php
+++ fuel/app/views/goal/index.php
@@ -1,182 +1,170 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>KIN TORE</title>
-    <?php echo Asset::css('home.css'); ?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.1/knockout-min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
-    <?php echo Asset::js('knockout-3.5.1.js')?>
-    <?php echo Asset::js('calendar.js')?> <!--カレンダのjsファイル-->
-    <?php echo Asset::js('record.js')?> <!--記録記入欄のjsファイル-->
-    <?php echo Asset::js('training.js')?> <!--記録記入欄のjsファイル-->
-    <?php echo Asset::js('counter.js')?>
-    <?php echo Asset::js('auth.js')?>
-    <?php echo Asset::js('log_edit.js')?>
-   
+    <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>KIN TORE</title>
+        <?php echo Asset::css('home.css'); ?>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+    
+        <!--自作JSファイル-->
+        <?php echo Asset::js('calendar.js')?> <!--カレンダのjs-->
+        <?php echo Asset::js('record.js')?> <!--記録記入欄のjs-->
+        <?php echo Asset::js('training.js')?> <!--記録用のjs-->
+        <?php echo Asset::js('counter.js')?> <!--カウンタ機能のjs-->
+        <?php echo Asset::js('log_edit.js')?> <!--ログ編集用のjs-->
+    </head>
+    <body>  
+        <!--　ページのヘッダー　-->
+        <div class="top">
+            <h3>KIN TORE</h3>
+            <a href="<?php echo Uri::create('login/logout'); ?>" class="logout-button">LOGOUT</a>
+        </div>
+        <hr class ="custom-divider">
 
-</head>
-<body>
-
-
-
-<!--　ページのヘッダー　-->
-<div class="top">
-        <h3>KIN TORE</h3>
-        <a href="<?php echo Uri::create('login/logout'); ?>" class="logout-button">LOGOUT</a>
-</div>
-<hr class ="custom-divider">
-
-<!--ゴール記入欄-->
-<div class ="welcome">
-    <h3>WELCOME <?php echo isset($username) ? htmlspecialchars(Session::get('username', 'USER')) : 'USER'; ?></h3>
-</div>
-
-<div class="goal-container">
-    <form action="<?php echo Uri::create('goal'); ?>" method="POST" class="goal-form">
-        <h1>目標:</h1>
-        <input type="text" class="input-box" name="goal" id="goal" 
-            value="<?php echo isset($latest_goal['goal']) ? $latest_goal['goal'] : ''; ?>" required>
-        <button type="submit" class="input-button" id="edit-goal-button">
-         <?php echo Asset::img('edit.png', ['alt' => 'Edit', 'style' => 'height: 40px; width: 40px;']); ?>
-        </button>
-    </form>
-</div>
-
-<div class = "header-wrapper">
-
-<!--カレンダー-->
-    <div class ="header-container">
-        <button id="prev" class="arrow-left">
-            <?php echo Asset::img('arrow.png', ['alt' => 'left arrow', 'style' => 'height: 30px; width: 30px;']); ?>
-        </button> 
-            <p class="current-date">12月</p>
-        <button id="next" class="arrow-right">
-            <?php echo Asset::img('right-arrow.png', ['alt' => 'right arrow', 'style' => 'height: 30px; width: 30px;']); ?>
-        </button>
-    </div>
-
-<!--登録した日のカウンターとビッグ３を表示する-->
-    <div class = "counter-container">
-        <p class= "counter"></p>
-    </div>
-
-    <div class="weight-container">
-        <p class = "CurWeight">体重：<?php echo isset($latest_weight['weight']) ? $latest_weight['weight'] : ''; ?>kg</p>
-    </div>
-    <div class="big3-container">
-        <p class = "CurBig3"> BIG 3</p>
-    </div>
-</div> 
-
-<div class="calendar-log-container">
-    <div class="wrapper">
-        <div class="calendar">
-            
-            <ul class="weeks">
-                <li>Sun</li>
-                <li>Mon</li>
-                <li>Tue</li>
-                <li>Wed</li>
-                <li>Thu</li>
-                <li>Fri</li>
-                <li>Sat</li>
-            </ul>
-            <ul class="days">
-            </ul>
-        </div>   
-    </div>
-
-
-<!--ログの記録を表示できる-->
-    <div class="scrollable-container">
-        <div class="log-data">
-            <h2 id="date-log"></h2>
-            <h2 id="training-log"></h2>
+        <!--ゴール記入欄-->
+        <div class ="welcome">
+            <h3>WELCOME <?php echo isset($username) ? htmlspecialchars(Session::get('username', 'USER')) : 'USER'; ?></h3>
         </div>
 
-        <div class= "menu-wrapper">
-            <h3 id ="exercise-name">筋トレ記録ログ欄</h3>
-            <h3 id="exercise-name" data-bind="text: exercise">日付を選択してください</h3>
-        </div>
-    <!--ボタン-->
-    <button type="button" class="log_edit" >編集</button>
-    <button type="button" class="log_delete" >削除</button>
-    </div>
-</div>
-
-
-<?php
-$deleteImg = Asset::get_file('x-mark.png', 'img');
-$addImg = Asset::get_file('add.png', 'img');
-?>
-
-  <!--部位の選択-->
-<div class="nav-bar">
-    <button class="nav-button" data-training="胸">胸</button>
-    <button class="nav-button" data-training="肩">肩</button>
-    <button class="nav-button" data-training="背中">背中</button>
-    <button class="nav-button" data-training="足">足</button>
-    <button class="nav-button" data-training="腕">腕</button>
-</div>
-
-<!-- 画像のURLをデータ属性に格納する-->
-<div class="input-container"
-     data-delete-img="<?= htmlspecialchars($deleteImg, ENT_QUOTES, 'UTF-8') ?>"
-     data-add-img="<?= htmlspecialchars($addImg, ENT_QUOTES, 'UTF-8') ?>">
-    <div class= "training-container" >
-        <div class="form-container">
-            <form id="training-form" action="<?php echo Uri::create('training/save'); ?>" method="POST">
-                <input type="hidden" id="selected-training" name="trainings[0][training_type]">
-<!--種目のインプットフォーム-->
-                <div class="training-name-wrapper">
-                    <div class="training-name-container">
-                     <input type="text" id="name" name="trainings[0][name]" placeholder="種目" required>
-                    </div>
-                    <button type="delete" class="deletion">
-                        <?php echo Asset::img('x-mark.png', ['alt' => 'x-mark', 'style' => 'height: 40px; width: 40px;']); ?>
-                    </button>
-                </div>
-<!--回数と重量のインプットフォーム-->
-            
-                <div class="weight-rep-wrapper">
-                    <div class="training-details">
-                        <div class="sets">
-                            <span class="set-num">1</span>
-                            <input type="number" id="weight" name="trainings_detail[0][sets][0][weight]" placeholder="重量" > 
-                            <span class="separator">&nbsp;x&nbsp;</span>
-                         <input type="number" id="reps" name="trainings_detail[0][sets][0][reps]" placeholder="レップ数" > 
-                        </div>
-                    </div> 
-                    <button type="add-set" class ="add-rep">
-                        <?php echo Asset::img('add.png', ['alt' => 'add', 'style' => 'height: 40px; width: 40px;']); ?>
-                    </button>
-                 </div> 
-           
-                <button type="button" class="add-set">追加</button>
-                <button type="submit" class="submit-button">完了</button>
+        <!--ユーザーが設定したゴールを記入、編集フォーム-->
+        <div class="goal-container">
+            <form action="<?php echo Uri::create('goal'); ?>" method="POST" class="goal-form">
+                <h1>目標:</h1>
+                <input type="text" class="input-box" name="goal" id="goal" 
+                    value="<?php echo isset($latest_goal['goal']) ? $latest_goal['goal'] : ''; ?>" required>
+                <button type="submit" class="input-button" id="edit-goal-button">
+                <?php echo Asset::img('edit.png', ['alt' => 'Edit', 'style' => 'height: 40px; width: 40px;']); ?>
+                </button>
             </form>
         </div>
-    </div>
+        <!--カレンダーとトレーニングログ表示のセクション-->
+        <div class = "header-wrapper">
+            <!--カレンダー-->
+            <div class ="header-container">
+                <button id="prev" class="arrow-left">
+                    <?php echo Asset::img('arrow.png', ['alt' => 'left arrow', 'style' => 'height: 30px; width: 30px;']); ?>
+                </button> 
+                    <p class="current-date">12月</p>
+                <button id="next" class="arrow-right">
+                    <?php echo Asset::img('right-arrow.png', ['alt' => 'right arrow', 'style' => 'height: 30px; width: 30px;']); ?>
+                </button>
+            </div>
 
-<div class="weight-input">
-    <form action="<?php echo Uri::create('weight/add_weight'); ?>" method="POST" class="weight-form" id=weight-form>
-        <div class = weight-input-container>
-                <label for="weight"> 体  重</label>
+            <!--登録した日のカウンターとビッグ３を表示する-->
+            <div class = "counter-container">
+                <p class= "counter"></p>
+            </div>
+
+            <!--体重表示-->
+            <div class="weight-container">
+                <p class = "CurWeight">体重：<?php echo isset($latest_weight['weight']) ? $latest_weight['weight'] : ''; ?>kg</p>
+            </div>
+             <!--BIG３合計表示-->
+            <div class="big3-container">
+                <p class = "CurBig3"> BIG 3</p>
+            </div>
+        </div> 
+
+        <!--カレンダー表示-->
+        <div class="calendar-log-container">
+            <div class="wrapper">
+                <div class="calendar">
+                    <ul class="weeks">
+                        <li>Sun</li>
+                        <li>Mon</li>
+                        <li>Tue</li>
+                        <li>Wed</li>
+                        <li>Thu</li>
+                        <li>Fri</li>
+                        <li>Sat</li>
+                    </ul>
+                    <ul class="days">
+                    </ul>
+                </div>   
+            </div>
+
+            <!--ログの記録を表示できる-->
+            <div class="scrollable-container">
+                <div class="log-data">
+                    <h2 id="date-log"></h2>
+                    <h2 id="training-log"></h2>
+                </div>
+            <!--ログ欄-->
+                <div class= "menu-wrapper">
+                    <h3 id ="exercise-name">筋トレ記録ログ欄</h3>
+                    <h3 id="exercise-name" data-bind="text: exercise">日付を選択してください</h3>
+                </div>
+            <!--ボタン-->
+                <button type="button" class="log_edit" >編集</button>
+                <button type="button" class="log_delete" >削除</button>
+            </div>
+        </div>  
+
+
+        <?php
+        $deleteImg = Asset::get_file('x-mark.png', 'img');
+        $addImg = Asset::get_file('add.png', 'img');
+        ?>
+
+        <!--部位の選択-->
+        <div class="nav-bar">
+            <button class="nav-button" data-training="胸">胸</button>
+            <button class="nav-button" data-training="肩">肩</button>
+            <button class="nav-button" data-training="背中">背中</button>
+            <button class="nav-button" data-training="足">足</button>
+            <button class="nav-button" data-training="腕">腕</button>
         </div>
-        <input type="number" class="input-box" name="user_weight" id="user-weight" 
-            value="<?php echo isset($latest_weight['user_weight']) ? $latest_weight['user_weight'] : ''; ?>" required>
-        <button type="submit" class="submit-weight-button" id="submit-weight-button"> 完了</button>
-    </form>
 
-</div>
-
-</div>    
-
-
-
-</body>
-
+        <!-- 画像のURLをデータ属性に格納する-->
+        <div class="input-container"
+            data-delete-img="<?= htmlspecialchars($deleteImg, ENT_QUOTES, 'UTF-8') ?>"
+            data-add-img="<?= htmlspecialchars($addImg, ENT_QUOTES, 'UTF-8') ?>">
+            <div class= "training-container" >
+                <div class="form-container">
+                    <!--種目のインプットフォーム-->
+                    <form id="training-form" action="<?php echo Uri::create('training/save'); ?>" method="POST">
+                        <input type="hidden" id="selected-training" name="trainings[0][training_type]">
+                            <div class="training-name-wrapper">
+                                <div class="training-name-container">
+                                <input type="text" id="name" name="trainings[0][name]" placeholder="種目" required>
+                                </div>
+                                <button type="delete" class="deletion">
+                                    <?php echo Asset::img('x-mark.png', ['alt' => 'x-mark', 'style' => 'height: 40px; width: 40px;']); ?>
+                                </button>
+                            </div>
+                        <!--回数と重量のインプットフォーム-->
+                        <div class="weight-rep-wrapper">
+                            <div class="training-details">
+                                <div class="sets">
+                                    <span class="set-num">1</span>
+                                    <input type="number" id="weight" name="trainings_detail[0][sets][0][weight]" placeholder="重量" > 
+                                    <span class="separator">&nbsp;x&nbsp;</span>
+                                    <input type="number" id="reps" name="trainings_detail[0][sets][0][reps]" placeholder="レップ数" > 
+                                </div>
+                            </div> 
+                            <button type="add-set" class ="add-rep">
+                                <?php echo Asset::img('add.png', ['alt' => 'add', 'style' => 'height: 40px; width: 40px;']); ?>
+                            </button>
+                        </div> 
+                
+                        <button type="button" class="add-set">追加</button>
+                        <button type="submit" class="submit-button">完了</button>
+                    </form>
+                </div>
+            </div>
+            <!--体重のインプット欄-->
+            <div class="weight-input">
+                <form action="<?php echo Uri::create('weight/add_weight'); ?>" method="POST" class="weight-form" id=weight-form>
+                    <div class = weight-input-container>
+                            <label for="weight"> 体  重</label>
+                    </div>
+                    <input type="number" class="input-box" name="user_weight" id="user-weight" 
+                        value="<?php echo isset($latest_weight['user_weight']) ? $latest_weight['user_weight'] : ''; ?>" required>
+                    <button type="submit" class="submit-weight-button" id="submit-weight-button"> 完了</button>
+                </form>
+            </div>
+        </div>    
+    </body>
 </html>
 
