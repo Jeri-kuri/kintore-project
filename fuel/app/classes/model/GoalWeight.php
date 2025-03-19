0fd2c4a3a59733722ae81582627be6f98a6a9b88
Jeri-kuri <komiyajerico@gmail.com>
2025-03-19 10:09:50 +0900

    adding comments to the model files to add readability

--- fuel/app/classes/model/GoalWeight.php
+++ fuel/app/classes/model/GoalWeight.php
@@ -1,23 +1,37 @@
 <?php
 
+/**
+ * Summary of Model_GoalWeight
+ * ゴールと体重データを取得するためのモデルクラス
+ */
 class Model_GoalWeight extends Model
 {
+    /**
+     * Summary of get_latest_goal
+     * データベースから最新のゴールを取得する
+     * @return array|null 最新のゴールデータを配列で返す。データが存在しない場合は null を返す。
+     */
     public static function get_latest_goal()
     {
         return DB::select('goal')
             ->from('goal')
-            ->order_by('date_inserted', 'desc')
-            ->limit(1)
+            ->order_by('date_inserted', 'desc')//挿入日時が新しい順にする
+            ->limit(1)//最新の一件のみ取得
             ->execute()
             ->current();
     }
 
+    /**
+     * Summary of get_latest_weight
+     * データベースから最新の体重を取得する
+     * @return array|null 最新の体重のデータを配列で返す。データが存在しない場合は null を返す。
+     */
     public static function get_latest_weight()
     {
         return DB::select('weight')
             ->from('weight')
-            ->order_by('created_date', 'desc')
-            ->limit(1)
+            ->order_by('created_date', 'desc')//挿入日時が新しい順にする
+            ->limit(1)//最新の一件のみ取得
             ->execute()
             ->current();
     }
--- fuel/app/classes/model/traininglog.php
+++ fuel/app/classes/model/traininglog.php
@@ -1,14 +1,23 @@
 <?php
 
+/**
+ * Summary of Model_Traininglog
+ * トレーニングログに関連するデータを取得するためのモデルクラス
+ */
 class Model_Traininglog extends Model
 {
+    /**
+     * Summary of get_workout_log_by_date
+     * @param mixed $date
+     * @return array トレーニングデータ（カテゴリー）を含む配列。該当するデータがなければ空の配列を返す。
+     * 指定した日付のトレーニングデータを取得する
+     */
     public static function get_workout_log_by_date($date)
     {
-
         return DB::select('category')
         ->from('exercise_type')
-        ->where('created_at', '=', $date) 
-        ->order_by('created_at', 'desc')  
+        ->where('created_at', '=', $date) //指定された日付のデータを絞り込み
+        ->order_by('created_at', 'desc')  //作成された日の新しい順に並び変える
         ->limit(1)  
         ->execute()
         ->as_array();
